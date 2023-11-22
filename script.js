edf8747ba7ed9d36a620679f8c9a262416aa4ba3
Phillip Clayton <36546218+PhillipClayton@users.noreply.github.com>
2023-11-22 15:58:49 -0700

    Update script.js again

--- script.js
+++ script.js
@@ -56,14 +56,15 @@ function getRandomWordsFromArray(wordsArray) {
 
 // Function to shuffle an array
 function shuffleArray(arrayToShuffle) {
-    let shuffledArray = arrayToShuffle;
+    let shuffledArray = arrayToShuffle.slice();
+
     for (let i = shuffledArray.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
     }
+
     return shuffledArray;
 }
-
 // Generate word buttons in the "wordDisplay" div
 function generateWordButtons(words) {
     const wordButtonsContainer = document.getElementById('wordButtons');
