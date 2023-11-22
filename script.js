9798ab8ea9cc8ace63e59ef4c12f8c104f6f8185
Phillip Clayton <36546218+PhillipClayton@users.noreply.github.com>
2023-11-22 20:13:50 +0000

    Changed folder name, values of dropdown, etc

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// Function to load the word bank for a given grade from a CSV file
+async function loadWordBank(grade) {
+    try {
+        const response = await fetch(`WordBanks/grade${grade}.csv`);
+        const csvData = await response.text();
+        return parseCSV(csvData);
+    } catch (error) {
+        throw new Error(`Error loading word bank for grade ${grade}: ${error.message}`);
+    }
+}
+
 // Function to speak a given word using the Web Speech API
 function speakWord(word) {
     // Check if the browser supports the SpeechSynthesis API
@@ -18,6 +29,61 @@ function speakWord(word) {
     }
 }
 
-// Example usage
+/* Example usage
 const wordToSpeak = 'apple';
 speakWord(wordToSpeak);
+*/
+
+/* Example usage in the context of your application
+const wordButton = document.getElementById('wordButton1'); // Replace with the actual ID of your button
+wordButton.addEventListener('click', () => {
+    const wordToSpeak = 'apple'; // Replace with the actual word associated with the button
+    speakWord(wordToSpeak);
+});
+*/ 
+
+// Function to load word bank for a specific grade
+async function loadWordBank(grade) {
+    try {
+        const response = await fetch(`wordbanks/grade${grade}.csv`);
+        const data = await response.text();
+        return parseCSV(data);
+    } catch (error) {
+        console.error('Error loading word bank:', error);
+    }
+}
+
+// Function to parse CSV data
+function parseCSV(csvData) {
+    const lines = csvData.split('\n');
+    const headers = lines[0].split(',');
+
+    const wordBank = [];
+
+    for (let i = 1; i < lines.length; i++) {
+        const values = lines[i].split(',');
+        const entry = {};
+
+        for (let j = 0; j < headers.length; j++) {
+            entry[headers[j]] = values[j];
+        }
+
+        wordBank.push(entry);
+    }
+
+    return wordBank;
+}
+
+// Button listener for displaying the buttons for spelling
+document.addEventListener('DOMContentLoaded', function () {
+    const startLearningBtn = document.getElementById('startLearningBtn');
+    
+    startLearningBtn.addEventListener('click', function () {
+        // Get the selected grade value
+        const gradeSelect = document.getElementById('gradeSelect');
+        const selectedGrade = gradeSelect.value;
+        
+        // Call your function with the selected grade as an argument
+        loadWordBank(selectedGrade);
+    });
+});
